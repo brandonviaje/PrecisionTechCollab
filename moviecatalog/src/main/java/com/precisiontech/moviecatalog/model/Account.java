7044f1f0b68a37c315fb0f494bf175df1215471b
brandonviaje <brandon.viaje@ontariotechu.net>
2025-04-05 18:08:24 -0400

    added comments

--- moviecatalog/src/main/java/com/precisiontech/moviecatalog/model/Account.java
+++ moviecatalog/src/main/java/com/precisiontech/moviecatalog/model/Account.java
@@ -20,39 +20,90 @@ public class Account {
         this.password = password;
     }
 
-    // Getters and setters
+
+    // ------------- Getters and Setters -------------
+
+
+    /**
+     * Gets the user's full name.
+     *
+     * @return the full name as a String
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Sets the user's full name.
+     *
+     * @param fullName      the new full name
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * Gets the user's username.
+     *
+     * @return the username as a String
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the user's username.
+     *
+     * @param username      the new username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets the user's password.
+     *
+     * @return the password as a String
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the user's password.
+     *
+     * @param password      the new password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Gets the user's account id.
+     *
+     * @return the id as a String
+     */
     public String getAccountId() {
         return accountId;
     }
 
+    /**
+     * Sets the user's account id.
+     *
+     * @param accountId      the new account id
+     */
     public void setAccountId(String accountId) {
         this.accountId = accountId;
     }
 
+    // ------------- Helper Functions -------------
+
+
+    /**
+     * Gets a string representation of the {@code Account} object
+     *
+     * @return a string containing the account details
+     */
     @Override
     public String toString() {
         return "Account{" +
